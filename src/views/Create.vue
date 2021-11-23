0dfa35ebd542ecfc24f6f329aa4b12f5b124a1cf
andrew <r04631044@ntu.edu.tw>
2021-11-23 17:04:06 +0800

    img upload complete

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,42 +1,45 @@
 <template>
   <div class="create mt-5">
     <h3>新增商品</h3>
-    <section class="py-3">
-      <div class="container">
-          <div class="row">
-            <form id="createProductForm" v-on:submit.prevent="createForm($event)" class="container col-md-6 py-3">
-                <div class="form-group">
+    <section class="py-3 section">
+      <div class="">
+          <div class="">
+            <form v-on:submit.prevent="createForm($event)" class="createProductForm">
+                <div class="create-form-group">
                     <label for="productName">商品名稱</label>
                     <input type="text" v-model="product.name" id="productName" class="form-control" required>
                 </div>
-                <div class="form-group">
+                <div class="create-form-group">
                     <label for="productPrice">商品價格</label>
                     <input type="number" v-model="product.price" id="productPrice" class="form-control" min="1" max="90000" required>
                 </div>
                 <div class="upload">
                   <div class="upload-head">
-                      <label class="btn btn-primary">
+                      <label class="btn btn-primary mb-0">
                         <input id="upload_img" style="display:none;" accept="image/*" @change="previewImage" type="file" multiple>
                          選擇圖片
                       </label>
-                      <button class="btn btn-primary ml-3" @click="upload">上傳圖片</button>
+                      <button type="click" class="btn btn-primary ml-3" @click.prevent="upload">上傳圖片</button>
                       <button class="btn btn-primary ml-3">Cancel</button>
                   </div>
-                  <div class="upload-img">
-                    <ScrollPanel class="scroll-panel">
-                      <tbody id="cartTableBody" v-for="item, index in preview" :key="index">
-                        <tr>
-                            <td class="text-right">
-                              <img :src="item" class=""  alt="圖片未顯示">
-                            </td>
-                            <td class="text-right"> {{ imgList[index].name}}</td>
-                            <td class="text-right ml-3">size: {{ imgList[index].size/1024 }}KB</td>
-                        </tr>
-                      </tbody>
-                    </ScrollPanel>
+                  <div class="upload-img">             
+                      <table id="cartTableBody">
+                        <ScrollPanel class="scroll-panel">
+                          <tr v-for="item, index in preview" :key="index">
+                              <td class="text-left td-img">
+                                <img :src="item" class="" alt="圖片未顯示">
+                              </td>
+                              <td class="text-left td-img"> {{ imgList[index].name}}</td>
+                              <td class="text-left td-img">size: {{ Math.round(imgList[index].size/1024  * 10) / 10 }}KB</td>
+                              <td class="td-img row align-items-center justify-content-center">
+                                <Button @click.prevent="deleteItem(index)" icon="pi pi-times" class="p-button-rounded p-button-danger p-button-text" />
+                              </td>                 
+                          </tr>
+                        </ScrollPanel>
+                      </table>      
                   </div>
                 </div>
-                <div class="form-group">
+                <div class="create-form-group">
                     <label for="productCategory">商品分類</label>
                     <select v-model="product.category" id="productCategory" class="form-control">
                         <option value="書架喇叭">書架喇叭</option>
@@ -46,7 +49,7 @@
                         <option value="落地喇叭">落地喇叭</option>
                     </select>
                 </div>
-                <div class="form-group my-4">
+                <div class="create-form-group my-4">
                     <button type="submit" class="btn btn-primary"><i class="fas fa-plus"></i> 新增商品</button>
                 </div>
             </form>
@@ -71,15 +74,11 @@ export default {
         image:[],
         category: ''
       },
-      api:''
+      api:'insert'
     }
   }, 
   methods: {
     createForm () {
-          //讀取圖片路徑
-            this.image.forEach(img=>{
-              this.product.image.push(img.name)
-            })
             const product = {
               name: this.product.name,
               price: this.product.price,
@@ -87,49 +86,70 @@ export default {
               category: this.product.category ,
               createdAt: new Date().getTime()
             }
-              const config = {
-                headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          }
-              };
-              axios.post(`https://x-home.pcpogo.com/px/${this.api}.php?PDEBUG=andrewc`, product, config)
-                  .then(response => {
-                    console.log('res  ', response);
-                  })
-                  .catch(error => {
-                    console.log('err',error);
-                  });
+            const config = {
+              headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+              }
+            };
+            axios.post(`https://x-home.pcpogo.com/px/${this.api}.php?PDEBUG=andrewc`, product, config)
+                .then(response => {
+                  console.log('res  ', response);
+                })
+                .catch(error => {
+                  console.log('err',error);
+                });
     },
     previewImage (event) {
       var input = event.target;
-      var count = input.files.length;
-      var index = 0;
-      if (input.files) {
-        while(count --) {
+
+      console.log('input.files',input.files)
+   
           var reader = new FileReader();
           reader.onload = (e) => {
             this.preview.push(e.target.result);
           }
-          this.imgList.push(input.files[index]);
-          reader.readAsDataURL(input.files[index]);
-          index ++;
-        }
-      }     
+          console.log('input.files[0]',input.files[0])
+          this.imgList.push(input.files[0]);       
+          reader.readAsDataURL(input.files[0]);
+      
     },
     upload(){
-      // console.log('preview',this.preview)
-      console.log('preview',this.imgList)
-
+                //讀取圖片路徑
+      this.imgList.forEach(img=>{
+        this.product.image.push(img.name)
+      })
+      console.log('product',this.product)
+    },
+    deleteItem(idx){
+      this.preview.splice(idx,1)
+      this.imgList.splice(idx,1)
     }
   }
 }
 </script>
 <style scoped>
+.section{
+  display: flex;
+  justify-content: center;
+}
+
+.createProductForm{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  max-width:650px;
+}
+
+.create-form-group{
+  width: 500px;
+  padding: 20px 40px;
+}
 
 .upload{
   border: 1px solid black;
   box-sizing: border-box;
-  width: 500px;
+  margin: 0;
+  width: 800px;
 }
 
 .upload-head{
@@ -143,8 +163,9 @@ export default {
 
 .upload-img{
   height: 400px;
-  margin: 0;
   padding: 20px;
+  display: flex;
+  justify-content: center;
 }
 
 .upload-img img{
@@ -156,6 +177,15 @@ export default {
 .scroll-panel{
   height: 350px;
 }
+
+#cartTableBody{
+  width: 100%;
+}
+
+.td-img{
+  width: 150px;
+}
+
 
 </style>
 
