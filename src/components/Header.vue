fcb8e7d9d8a1669740a9239ffa94253ac37984c2
andrew <r04631044@ntu.edu.tw>
2021-12-30 17:01:43 +0800

    different header img using vuex failed

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,34 @@
 <template>
-    <header class="header">
+    <header class="header" :style="inlineBgImage(src)">
 
     </header>
 </template>
 <script>
+import { ref} from "vue";
+// import store from "@/store";
 export default {
+    setup(){
+        // const src = computed(()=>{
+        // // 監聽購物車商品數量
+        //     return store.state.src;
+        // })
+        const src = ref("header1.jpg")
+        function inlineBgImage(image) {
+            let bgImage = require('@/assets/img/' + image)
 
+            return {
+                backgroundImage: `url("${bgImage}")`,
+            }
+        }
+
+        return { inlineBgImage  , src}
+    }
 }
 </script>
 <style scoped>
 .header{
     width: 100%;
     height: 600px;
-    background-image: url("../assets/img/header1.jpg");
     background-position: center;
     background-attachment:fixed;
     background-size: cover;
