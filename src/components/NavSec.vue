0a02554eea72b5c6eb899fcdc0f165bc572d6b88
andrew <r04631044@ntu.edu.tw>
2022-04-20 11:45:14 +0800

    independent the section to NavSec.vue from navBar.vue

--- src/components/NavSec.vue
+++ src/components/NavSec.vue
@@ -16,16 +16,225 @@
             </div>
             <i v-if="isLogin" @click="toUserInfo" class="pi pi-user i-custom" type="button" aria-current="page"></i>     
         </div>
-        <button @click="toggleBar()" v-click-outside="onClickOutside" class="nav-toggler" style="cursor:pointer;">
-            <div class="line"></div>
-        </button>
     </section>
+    <transition>
+        <Modal v-if="isClickCart" @closeBtn="closeModal" />
+    </transition>
 </template>
 <script>
+import { ref,computed,onMounted,defineAsyncComponent} from "vue";
+import store from "@/store";
+import Search from '@/views/Search'
+import UserInfo from '@/views/UserInfo'
+import Login from '@/views/Login'
+import { useRouter,useRoute } from "vue-router";
 export default {
-    setup(){
-        
-        return {}
+    emits: ["searchColor"], 
+    components: {
+        Modal: defineAsyncComponent(() => import("@/components/Modal.vue")),
+    },
+    setup(props, { emit }){
+        const router = useRouter();
+        const route = useRoute();
+        const isHoverCart = ref(false);
+        const isLogin = ref(true);
+        const myinput = ref(null);
+        const isClickCart = ref(false);
+        const searchBar = ref("");
+        const isExtend = ref(false);
+        const loginState = computed(()=>{
+            return store.getters.loginState;
+        })
+        const changeCartNum = computed(()=>{
+            // 監聽購物車商品數量
+            return store.getters.changeCartNum;
+        })
+
+        onMounted(()=>{
+            // 取得購物車商品數量
+            store.dispatch("DataGetCart");
+            // const itemListStr = localStorage.getItem("cart");
+            // const defaultList = JSON.parse(itemListStr);
+            // store.state.itemList = defaultList || []; 
+        })
+        function openModal () {
+            document.body.style.overflow = 'hidden'
+            isClickCart.value = true
+        }
+        function closeModal(){
+            document.body.style.overflow = 'auto'
+            isClickCart.value = false
+        }
+        function goIndex(){
+            router.push({
+                path: `/`,
+            }) 
+        }
+        function innerSearch(){
+            let id = searchBar.value
+            if(id&& route.path!==`/search/${id}` &&route.name!=='search'){
+                router.push({
+                    path: `/search/${id}`,
+                    component: Search,
+                })  
+            }else if(id&&route.name=='search'){
+                router.push({
+                    path: `/search/${id}`,
+                    component: Search,
+                })  
+            }
+        }
+        function showInput(){
+            emit("searchColor");
+            isExtend.value = true
+            myinput.value.focus()
+        }   
+        function blurFocus(){
+            emit("searchColor");
+            isExtend.value =false
+        }
+        function toUserInfo(){
+            if(Object.entries(loginState.value).length !==0){
+                router.push({
+                    path: `/userinfo`,
+                    component: UserInfo,
+                }) 
+            }else{
+                router.push({
+                    path: `/login`,
+                    component: Login,
+                }) 
+            }
+        }
+        return {
+            goIndex,
+            innerSearch,
+            openModal,
+            closeModal,
+            showInput,
+            blurFocus,
+            toUserInfo,
+            changeCartNum,
+            isHoverCart,
+            isLogin,
+            isClickCart,
+            searchBar,
+            isExtend,
+            myinput,
+        }
     }
 }
 </script>
+<style scoped>
+.section{
+    width: 100%;
+    height: 75px;
+    background: white;
+    position: fixed;
+    margin-top: 45px;
+    z-index: 48;
+    transition: .3s ease;
+}
+
+.v-enter-active,.v-leave-active {
+    transition: opacity .4s;
+}
+
+.v-enter-from, .v-leave-to {
+    opacity: 0;
+}
+
+.work-brench{
+    position: fixed;
+    right: 20%;
+}
+
+.work-brench2{
+    position: fixed;
+    left: 20%;
+}
+
+.search-bar{
+    display: flex; 
+    align-items: center; 
+}
+
+.s-input{
+    position: relative;
+    left: 30px;
+    width: 0px;
+    height: 38px;
+    opacity: 0;
+    border-radius: 6px;
+    transition: .3s ease-in;
+    border:none;
+}
+
+.pi-search{
+    position: relative;
+    z-index: 2;
+    /* left: -40px; */
+}
+
+.s-input.show {
+    width: 220px;
+    transition: .2s ease-in;
+    opacity: 1;
+} 
+
+input:focus{
+    outline:none;
+    background: rgb(233, 232, 232);
+    transition: .5s auto;
+}
+
+input::-webkit-search-cancel-button{
+    display: none;
+}
+
+.cart-place{
+    position: relative;
+    transition: .3s;
+}
+
+.cart-place.move{
+    transform: scale(1.2);
+}
+
+#cartNumber {
+    width: 28px;
+    height: 28px;
+    background-color: tomato;
+    text-align: center;
+    line-height: 30px;
+    position: absolute;
+    border-radius: 50%;
+    top: -10px;
+    left: 12px;
+    color: white;
+    transform: scale(.6);
+}
+
+.i-custom{
+    font-size: 1.6rem;
+    cursor: pointer;
+}
+
+@media (max-width: 900px){
+    .pi-slack{
+        display: none;
+    }
+
+    .work-brench2{
+        display: none;
+    }
+
+    .section{
+        border-bottom: 1px solid rgb(243, 243, 243) ;
+    }
+
+    .work-brench{
+        right: 10%;
+    }
+}
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,27 +1,7 @@
 <template>
     <header class="header p-d-flex p-ai-center p-jc-center">台灣本島免運費</header>
-    <section class="section p-d-flex p-ai-center p-jc-center">
-        <div class="work-brench2">
-            <i class="pi pi-map-marker i-custom p-mr-5"></i>
-            <i class="pi pi-heart i-custom"></i>
-        </div>
-        <i @click="goIndex()" class="pi pi-slack p-ml-lg-2" style="fontSize: 2.5rem;cursor:pointer;"></i>
-        <div class="work-brench p-d-flex p-ai-center">
-            <form @submit.prevent="innerSearch($event)" class="search-bar p-mr-5">  
-                <input v-model="searchBar" ref="myinput" @blur="blurFocus" :class="{ show: isExtend }" class="s-input" type="search" placeholder="Search" aria-label="Search">
-                <i class="pi pi-search i-custom" @click="showInput" type="button"></i>          
-            </form>
-            <div class="cart-place p-mr-5" @click.prevent="openModal()" @mouseover="isHoverCart = true" @mouseleave="isHoverCart=false" v-bind:class="{ move: isHoverCart }">
-                <div id="cartNumber" type="button" style="cursor:pointer;">{{changeCartNum}}</div>
-                <i class="pi pi-shopping-cart i-custom" type="button"></i>  
-            </div>
-            <i v-if="isLogin" @click="toUserInfo" class="pi pi-user i-custom" type="button" aria-current="page"></i>     
-        </div>
-        <button @click="toggleBar()" v-click-outside="onClickOutside" class="nav-toggler" style="cursor:pointer;">
-            <div class="line"></div>
-        </button>
-    </section>
-    <nav :class="{ active: isActive }"  class="nav-top p-d-flex p-ai-center p-jc-center">  
+    <NavSec @searchColor="searchColor"/>
+    <nav :class="{ active: isActive }"  v-click-outside="onClickOutside" class="nav-top p-d-flex p-ai-center p-jc-center">  
         <div class="p-mr-3 close-btn">
             <Button @click="closeLeftMenu" icon="pi pi-times" class="p-button-rounded p-button-plain p-button-text" />  
         </div>
@@ -33,56 +13,37 @@
             </li>
         </ul>
     </nav>
+    <button @click="toggleBar()" class="nav-toggler" style="cursor:pointer;">
+        <div class="line"></div>
+    </button>
     <transition name="mask">
         <div class="DivOverlapMask" v-show="showMask"></div> 
-    </transition>
-    <transition>
-        <Modal v-if="isClickCart" @closeBtn="closeModal" />
     </transition>
 </template>
 <script>
 import store from "@/store";
+import NavSec from '@/components/NavSec'
 import Itembar from '@/components/Itembar'
-import Search from '@/views/Search'
-import UserInfo from '@/views/UserInfo'
-import Login from '@/views/Login'
-import { ref,computed,onMounted, reactive,defineAsyncComponent} from "vue";
-import { useRouter, useRoute } from "vue-router";
+import { ref,onMounted, reactive} from "vue";
 import { navBarList } from "@/js/navList.js";
 export default {  
-    name: 'Navbar',
     emits: ["searchColor"], 
     components: {
-        Modal: defineAsyncComponent(() => import("@/components/Modal.vue")),
         Itembar,
+        NavSec
     },
     setup(props, { emit }){
+        const isActive = ref(false)
         const showMask = ref(false);
         const hoverNav = ref(false)
         const chooseNav = ref(Number);
         const textColor = ref(false);
-        const myinput = ref(null);
-        const router = useRouter();
-        const route = useRoute();
-        const isClickCart = ref(false);
-        const searchBar = ref("");
-        const isActive = ref(false);
-        const isExtend = ref(false);
         const barDrawer = ref(false);
-        const isHoverCart = ref(false);
-        const isLogin = ref(true);
         const navObj = reactive({
             title:'',
             content:[]
         })
         const navList = ref(navBarList)
-        const loginState = computed(()=>{
-            return store.getters.loginState;
-        })
-        const changeCartNum = computed(()=>{
-            // 監聽購物車商品數量
-            return store.getters.changeCartNum;
-        })
         
         onMounted(()=>{
             console.log('navList~~',navList.value)
@@ -92,6 +53,10 @@ export default {
             // const defaultList = JSON.parse(itemListStr);
             // store.state.itemList = defaultList || []; 
         })
+
+        function searchColor(){
+            emit("searchColor");
+        }
 
         function closeLeftMenu(){
             isActive.value=false
@@ -103,6 +68,7 @@ export default {
 
         function showUpMask(event){
             let idx = event.currentTarget.id
+            console.log('idx',idx)
             navObj.title = navList.value[idx].title
             navObj.content = navList.value[idx].content
             hoverNav.value = true
@@ -115,64 +81,10 @@ export default {
             }
         }
 
-        function openModal () {
-            document.body.style.overflow = 'hidden'
-            isClickCart.value = true
-        }
-        function closeModal(){
-            document.body.style.overflow = 'auto'
-            isClickCart.value = false
-        }
-        function goIndex(){
-            router.push({
-                path: `/`,
-            }) 
-        }
-
-        function innerSearch(){
-            let id = searchBar.value
-            if(id&& route.path!==`/search/${id}` &&route.name!=='search'){
-                router.push({
-                    path: `/search/${id}`,
-                    component: Search,
-                })  
-            }else if(id&&route.name=='search'){
-                router.push({
-                    path: `/search/${id}`,
-                    component: Search,
-                })  
-            }
-        }
-
         function toggleBar(){
-            isActive.value = true;
+            isActive.value = true
         }
-
-        function showInput(){
-            emit("searchColor");
-            isExtend.value = true
-            myinput.value.focus()
-        }
-        
-        function blurFocus(){
-            emit("searchColor");
-            isExtend.value =false
-        }
-
-        function toUserInfo(){
-            if(Object.entries(loginState.value).length !==0){
-                router.push({
-                    path: `/userinfo`,
-                    component: UserInfo,
-                }) 
-            }else{
-                router.push({
-                    path: `/login`,
-                    component: Login,
-                }) 
-            }
-        }
-
+  
         function toItemBar(){
             barDrawer.value=true
         }
@@ -180,41 +92,32 @@ export default {
         function closeBarDrawer(){
             barDrawer.value=false
         }
+
+        function clickLink(idx){
+            navObj.title = navList.value[idx].title
+            navObj.content = navList.value[idx].content
+        }
         return {
             chooseNav,
-            myinput,
             textColor,
             navObj,
             navList,
             hoverNav,
             showUpMask,
-            isClickCart,
-            searchBar,
-            openModal,
-            closeModal,
-            changeCartNum,
-            innerSearch, 
-            isActive,
-            isExtend,
-            toggleBar,
-            isHoverCart,
-            barDrawer,
-            isLogin,
-            showInput,
-            blurFocus,
-            toUserInfo,
-            closeLeftMenu,
             toItemBar,
             closeBarDrawer,
             showMask,
-            goIndex,
-            onClickOutside
+            barDrawer,
+            onClickOutside,
+            isActive,
+            closeLeftMenu,
+            searchColor,
+            toggleBar,
+            clickLink
         }
     },
 }
-
 </script>
-
 <style scoped>
 .header{
     width: 100%;
@@ -223,16 +126,6 @@ export default {
     color: white;
     position: fixed;
     z-index: 50;
-}
-
-.section{
-    width: 100%;
-    height: 75px;
-    background: white;
-    position: fixed;
-    margin-top: 45px;
-    z-index: 50;
-    transition: .3s ease;
 }
 
 .nav-top {
@@ -248,14 +141,6 @@ export default {
 
 .close-btn{
     display: none;
-}
-
-.v-enter-active,.v-leave-active {
-    transition: opacity .4s;
-}
-
-.v-enter-from, .v-leave-to {
-    opacity: 0;
 }
 
 .nav-ul {
@@ -284,77 +169,6 @@ export default {
     display: none;
 }
 
-.work-brench{
-    position: fixed;
-    right: 20%;
-}
-
-.work-brench2{
-    position: fixed;
-    left: 20%;
-}
-
-.search-bar{
-    display: flex; 
-    align-items: center; 
-}
-
-.s-input{
-    position: relative;
-    left: 30px;
-    width: 0px;
-    height: 38px;
-    opacity: 0;
-    border-radius: 6px;
-    transition: .3s ease-in;
-    border:none;
-}
-
-.pi-search{
-    position: relative;
-    z-index: 2;
-    /* left: -40px; */
-}
-
-
-.s-input.show {
-    width: 220px;
-    transition: .2s ease-in;
-    opacity: 1;
-} 
-
-input:focus{
-    outline:none;
-    background: rgb(233, 232, 232);
-    transition: .5s auto;
-}
-
-input::-webkit-search-cancel-button{
-    display: none;
-}
-
-.cart-place{
-    position: relative;
-    transition: .3s;
-}
-
-.cart-place.move{
-    transform: scale(1.2);
-}
-
-#cartNumber {
-    width: 28px;
-    height: 28px;
-    background-color: tomato;
-    text-align: center;
-    line-height: 30px;
-    position: absolute;
-    border-radius: 50%;
-    top: -10px;
-    left: 12px;
-    color: white;
-    transform: scale(.6);
-}
 .itemList:hover .itemBar{
     visibility: visible;
     opacity: 1;
@@ -389,25 +203,7 @@ input::-webkit-search-cancel-button{
     opacity: 0;
 }
 
-.i-custom{
-    font-size: 1.6rem;
-    cursor: pointer;
-}
-
-
 @media (max-width: 900px){
-    .pi-slack{
-        display: none;
-    }
-
-    .work-brench2{
-        display: none;
-    }
-
-    .section{
-        border-bottom: 1px solid rgb(243, 243, 243) ;
-    }
-
     .nav-top {
         position: fixed;
         top: 0px;
@@ -447,14 +243,15 @@ input::-webkit-search-cancel-button{
     }
 
     .nav-toggler{
+        display: block;
         position: fixed;
+        top: 50px;
         left: 10px;
         border: none;
-        display: block;
         width: 60px;
         height: 60px;
         background: transparent;
-        z-index: 500;
+        z-index: 49;
     }
 
     .line {
@@ -463,7 +260,6 @@ input::-webkit-search-cancel-button{
         background: rgb(0, 0, 0);
         position: relative;
     }
-
 
     .line:before,
     .line:after {
@@ -483,10 +279,6 @@ input::-webkit-search-cancel-button{
         bottom: -8px;
     }
 
-    .work-brench{
-        right: 10%;
-    }
-
     .itemBar{
         position: fixed;
         visibility: visible;
@@ -499,7 +291,7 @@ input::-webkit-search-cancel-button{
         background:rgb(255, 255, 255);
         margin-top:0;
         transform: translateX(0%);
-        transition-delay: 0s;
+        /* transition-delay: 0s; */
     }
 
     .itemList:hover .itemBar{
@@ -510,6 +302,4 @@ input::-webkit-search-cancel-button{
         left: 0;
     }
 }
-
-
 </style>
