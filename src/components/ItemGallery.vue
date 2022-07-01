c7ae28a50f963b9a8d7f2ea938fae1dba0d60909
andrew <r04631044@ntu.edu.tw>
2022-07-01 11:22:52 +0800

    move img-gallery from item.vue to item gallery.vue

---
+++ src/components/ItemGallery.vue
@@ -0,0 +1,207 @@
+<template>
+    <div class="p-md-6 p-col-12 p-d-flex p-flex-wrap p-jc-center">
+        <div class="showImg p-md-12 p-col-12 p-d-flex p-jc-center">
+            <Image v-if="!preLoad" class="focus" :src="changeImgSrc(focusIndex)" :key="focusIndex" alt="Image" width="400" preview />
+            <Skeleton v-if="preLoad" width="400px" height="400px" class="skeleton-img" />
+        </div>
+        <div class="p-d-flex p-ai-center p-md-12 p-jc-center">
+            <div class="slide-prev p-d-flex p-ai-center">
+                <i v-show="left" @click="swipeLeft" class="pi pi-chevron-left" style="font-size: 2rem"></i>
+            </div>
+            <div id="slideList" ref="content" class="p-d-flex slide-list p-mx-2">
+                <div v-for="(items,index) in slideData" :key="items.id" :id="`ae${index}`">                   
+                    <li v-for="(item,idx) in items" :key="item.id"
+                        @click="clickImg(index,idx)" 
+                        :id="`${idx}`"
+                        class="p-m-3"
+                        :class="{box:chooseImg==`ae${index}${idx}`}"
+                        >
+                        <img v-if="!preLoad" :src="require(`../assets/img/${item}`)" alt="">
+                    </li>   
+                </div>
+            </div>
+            <div class="slide-prev p-d-flex p-ai-center">
+                <i v-show="right" @click="swipeRight" class="pi pi-chevron-right" style="font-size: 2rem"></i>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        slideList: {
+            type: Array,
+        },
+        preLoad:{
+            type:Boolean
+        }
+    },
+    data () {
+        return {
+            slideData: [],
+            focusIndex:'',
+            left:false,
+            right:true,
+            chooseImg:'ae00',
+        }
+    },
+    created(){
+        
+    },
+    methods:{
+        scrollTo(element, scrollPixels, duration) {
+            let scroll;
+            const scrollPos = element.scrollLeft;
+            // Condition to check if scrolling is required
+            if ( !( (scrollPos === 0 || scrollPixels > 0) && (element.clientWidth + scrollPos === element.scrollWidth || scrollPixels < 0))) {
+                // Get the start timestamp
+                const startTime =
+                "now" in window.performance
+                    ? performance.now()
+                    : new Date().getTime();
+                const that = this
+                scroll = function(timestamp) {
+                    //Calculate the timeelapsed
+                    const timeElapsed = timestamp - startTime;
+                    //Calculate progress 
+                    const progress = Math.min(timeElapsed / duration, 1);
+                    //Set the scrolleft
+                    element.scrollLeft = scrollPos + scrollPixels * progress;
+                    that.showArrow(element.scrollLeft)
+                    //Check if elapsed time is less then duration then call the requestAnimation, otherwise exit
+                    if (timeElapsed < duration) {
+                        //Request for animation
+                        window.requestAnimationFrame(scroll);
+                    } else {
+                        return;
+                    }
+                }
+                window.requestAnimationFrame(scroll);
+            }
+        },
+        swipeLeft() {
+            const content = document.getElementById('slideList')
+            this.scrollTo(content, -400, 400);
+        },
+        swipeRight() {
+            const content = document.getElementById('slideList')
+            this.scrollTo(content, 400, 400);
+        },
+        clickImg(index,idx) {
+            console.log('index', this.focusIndex)
+            this.chooseImg=`ae${index}${idx}`
+            this.focusIndex = this.slideData[index][idx]
+        },
+        showArrow(distance){
+            if(distance>15){
+                this.left = true
+            }else{
+                this.left = false
+            }
+            const allLens = this.slideData.reduce((total, item) => {
+                const itemValue = item.length * 132
+                return total + itemValue;
+            }, 15);
+            if(distance>=allLens-640){
+                this.right = false
+            }else{
+                this.right = true
+            }
+        },
+        changeImgSrc(src){
+            return require(`../assets/img/${src}`)
+        },
+        parentMsg(idx){
+            this.chooseImg=`ae${idx}0`
+            this.focusIndex = this.slideData[idx][0]
+            const content = document.getElementById('slideList')
+            const distance = this.slideData.slice(0, idx).reduce((total, item) => {
+                const itemValue = item.length * 132
+                return total + itemValue;
+            }, 15);
+            console.log(distance)
+            this.showArrow(distance)
+            // 移動至絕對位置
+            content.scrollTo({ left:distance, behavior: 'smooth' })
+        }
+    },
+    watch:{
+        preLoad: {
+            immediate: true,
+            deep: true,
+            handler(newValue) {
+                if(newValue===false){
+                    this.slideData = this.slideList
+                    this.focusIndex = this.slideData[0][0]
+                }
+            }
+        }
+    }
+}
+</script>
+<style scoped>
+.slide-list{
+    background-color: rgb(255, 255, 255);
+    width: 60%;
+    cursor: pointer;
+    white-space: nowrap;
+    overflow: hidden;
+}
+
+.slide-list li {
+    display: inline-block;
+    opacity: 0.3;
+}
+
+.slide-list li.box{
+    opacity: 1;
+    border: black 4px solid;
+}
+
+.slide-prev{
+    height: 100px;
+    width: 30px;
+    background: transparent;
+    cursor: pointer;
+    transform: scale(.8);
+    transition: all .1s ease-in-out;
+}
+
+.slide-prev:hover{
+    transform: scale(1);
+}
+
+.slide-list img{
+    width: 100px;
+    height: 100px;
+}
+
+.showImg img{
+    width:400px;
+    height:400px;
+}
+
+.focus{
+    animation:change .2s ease-in;
+}
+
+@keyframes change {
+    0%{
+        opacity: 0;
+    }
+
+    100%{
+        opacity: 1;
+    }
+}
+
+@media(max-width:600px){
+    .slide-list{
+        width: 50%;
+    }
+    .slide-list img{
+        width: 50px;
+        height: 50px;
+    }
+}
+</style>
--- src/views/Item.vue
+++ src/views/Item.vue
@@ -1,31 +1,6 @@
 <template>
     <section class="p-p-3 section p-d-flex p-flex-wrap">
-        <div class="p-md-6 p-col-12 p-d-flex p-flex-wrap p-jc-center">
-            <div class="showImg p-md-12 p-col-12 p-d-flex p-jc-center">
-                <Image v-if="!preLoad" class="focus" :src="changeImgSrc(focusIndex)" :key="focusIndex" alt="Image" width="400" preview />
-                <Skeleton v-if="preLoad" width="400px" height="400px" class="skeleton-img" />
-            </div>
-            <div class="p-d-flex p-ai-center p-md-12 p-jc-center">
-                <div class="slide-prev p-d-flex p-ai-center">
-                    <i v-show="left" @click="swipeLeft" class="pi pi-chevron-left" style="font-size: 2rem"></i>
-                </div>
-                <div id="slideList" ref="content" class="p-d-flex slide-list p-mx-2">
-                    <div v-for="(items,index) in slideData" :key="items.id" :id="`ae${index}`">                   
-                        <li v-for="(item,idx) in items" :key="item.id"
-                            @click="clickImg(index,idx)" 
-                            :id="`${idx}`"
-                            class="p-m-3"
-                            :class="{box:chooseImg==`ae${index}${idx}`}"
-                            >
-                            <img v-if="!preLoad" :src="require(`../assets/img/${item}`)" alt="">
-                        </li>   
-                    </div>
-                </div>
-                <div class="slide-prev p-d-flex p-ai-center">
-                    <i v-show="right" @click="swipeRight" class="pi pi-chevron-right" style="font-size: 2rem"></i>
-                </div>
-            </div>
-        </div>
+        <ItemGallery :slideList="slideList" ref="child" :preLoad="preLoad"/>
         <div class="p-mt-md-5 p-md-5 p-col-12 p-d-flex p-jc-center">
             <form @submit.prevent="addItem($event)" class="p-md-7 p-col-12 p-text-left">
                 <h1 class="card-title p-mb-2">{{product.name}}</h1>
@@ -41,7 +16,7 @@
                             @mouseleave="showColor=false" 
                             class="colorBox p-mt-3 p-mr-1" 
                             :style="{ cursor: idx==3?'not-allowed':'pointer'}"
-                            :class="`${item}`">
+                            :class="item">
                                 <div v-show="showColor===idx" class="dialog">
                                     <p>{{item}}</p>
                                 </div>
@@ -91,9 +66,11 @@
 <script>
 import axios from 'axios'
 import PrdDetail from '@/components/PrdDetail'
+import ItemGallery from '@/components/ItemGallery'
 export default {
     components:{
-        PrdDetail
+        PrdDetail,
+        ItemGallery
     },
     props: {
         id: {
@@ -107,18 +84,14 @@ export default {
             amount:0,
             key:'cart',
             columnCnt:[],
-            slideData: [],
+            slideList: [],
             colorGroup:[],
-            focusIndex:'',
             preLoad:true,
-            left:false,
-            right:true,
             showColor:false,
-            chooseImg:'ae00',
             showDetail:false,
         }
     },
-    async created() {
+    created() {
         // console.log('id now',this.id)
         this.$store.dispatch("DataGetCart");
         const options = {
@@ -135,22 +108,18 @@ export default {
         const defaultList = JSON.parse(itemListStr);
         this.$store.state.itemList = defaultList || []; 
 
-        await axios(options)
+        axios(options)
             .then(response => {
                 const item = response.data.find(item=>{
                     return item.id == this.id
                 })
                 if(item){
-                    this.slideData = JSON.parse(item.image)
+                    this.slideList = JSON.parse(item.image)
                     this.colorGroup = JSON.parse(item.color)
                     this.product = item
                     this.product.color = ''
-                    // this.specification = JSON.parse(item.specification)
-                    // this.columnCnt = this.specification[0]
-                    // this.specification.splice(0,1)
-                }
-                this.focusIndex = this.slideData[0][0]
-                this.preLoad = false
+                    this.preLoad = false
+                }         
             })
             .catch(error => {
                 console.log('err',error);
@@ -166,7 +135,6 @@ export default {
             const already = this.$store.state.itemList.find(item => {
                 return item.id === this.product.id && item.color === this.product.color
             })
-            console.log('already',already)
             if (already) {
                 // 最後的數量= 已經擁有的數量+現在使用者輸入的數量
                 already.amount = parseInt(already.amount) + parseInt(this.amount)
@@ -182,82 +150,10 @@ export default {
             localStorage.setItem(this.key, itemListStr);
             this.$store.dispatch("productToData");
         },
-        scrollTo(element, scrollPixels, duration) {
-            let scroll;
-            const scrollPos = element.scrollLeft;
-            // Condition to check if scrolling is required
-            if ( !( (scrollPos === 0 || scrollPixels > 0) && (element.clientWidth + scrollPos === element.scrollWidth || scrollPixels < 0))) {
-                // Get the start timestamp
-                const startTime =
-                "now" in window.performance
-                    ? performance.now()
-                    : new Date().getTime();
-                const that = this
-                scroll = function(timestamp) {
-                    //Calculate the timeelapsed
-                    const timeElapsed = timestamp - startTime;
-                    //Calculate progress 
-                    const progress = Math.min(timeElapsed / duration, 1);
-                    //Set the scrolleft
-                    element.scrollLeft = scrollPos + scrollPixels * progress;
-                    that.showArrow(element.scrollLeft)
-                    //Check if elapsed time is less then duration then call the requestAnimation, otherwise exit
-                    if (timeElapsed < duration) {
-                        //Request for animation
-                        window.requestAnimationFrame(scroll);
-                    } else {
-                        return;
-                    }
-                }
-                window.requestAnimationFrame(scroll);
-            }
-        },
-        swipeLeft() {
-            const content = document.getElementById('slideList')
-            this.scrollTo(content, -400, 400);
-        },
-        swipeRight() {
-            const content = document.getElementById('slideList')
-            this.scrollTo(content, 400, 400);
-        },
-        clickImg(index,idx) {
-            console.log('index', this.focusIndex)
-            this.chooseImg=`ae${index}${idx}`
-            this.focusIndex = this.slideData[index][idx]
-        },
         colorScroll(idx,item){
-            this.chooseImg=`ae${idx}0`
-            this.focusIndex = this.slideData[idx][0]
+            this.$refs.child.parentMsg(idx);
             this.product.color = item
-            const content = document.getElementById('slideList')
-            const distance = this.slideData.slice(0, idx).reduce((total, item) => {
-                const itemValue = item.length * 132
-                return total + itemValue;
-            }, 15);
-            console.log(distance)
-            this.showArrow(distance)
-            // 移動至絕對位置
-            content.scrollTo({ left:distance, behavior: 'smooth' })
         },
-        showArrow(distance){
-            if(distance>15){
-                this.left = true
-            }else{
-                this.left = false
-            }
-            const allLens = this.slideData.reduce((total, item) => {
-                const itemValue = item.length * 132
-                return total + itemValue;
-            }, 15);
-            if(distance>=allLens-640){
-                this.right = false
-            }else{
-                this.right = true
-            }
-        },
-        changeImgSrc(src){
-            return require(`../assets/img/${src}`)
-        }
     }
 }
 </script>
@@ -271,46 +167,6 @@ ul {
     margin-top: 180px;
 }
 
-/* slide */
-.slide-list{
-    background-color: rgb(255, 255, 255);
-    width: 60%;
-    cursor: pointer;
-    white-space: nowrap;
-    overflow: hidden;
-}
-
-.slide-list li {
-    display: inline-block;
-    opacity: 0.3;
-}
-
-.slide-list li.box{
-    opacity: 1;
-    border: black 4px solid;
-}
-
-.slide-prev{
-    height: 100px;
-    width: 30px;
-    background: transparent;
-    cursor: pointer;
-    transform: scale(.8);
-    transition: all .1s ease-in-out;
-}
-
-.slide-prev:hover{
-    transform: scale(1);
-}
-
-.slide-list img{
-    width: 100px;
-    height: 100px;
-}
-
-.list-sec{
-    width: 700px;
-}
 
 .card-title{
     font-size: 30px;
@@ -337,20 +193,14 @@ ul {
 }
 
 .inputK{
-        position: relative;
     width:28px;
     height: 42px;
-    border-radius: 0;
     background: rgb(243, 242, 242);
     border: 1.5px solid rgb(224, 224, 224);
     border-top: none;
     border-bottom: none;
     padding: 5px;
     text-align: center
-}
-.showImg img{
-    width:400px;
-    height:400px;
 }
 
 .general-btn{
@@ -406,7 +256,6 @@ ul {
 
 .dialog{
     position: absolute;
-    display: block;
     text-align: center;
     border-radius: 6px;
     opacity: .8;
@@ -423,26 +272,12 @@ ul {
     font-size: 6px;
 }
 
-.focus{
-    animation:change .2s ease-in;
-}
-
 .pi-plus{
     transition: all .3s ease;
 }
 
 .pi-plus.rotate {
     transform: rotateZ(45deg);
-}
-
-@keyframes change {
-    0%{
-        opacity: 0;
-    }
-
-    100%{
-        opacity: 1;
-    }
 }
 
 .tech-area{
@@ -471,14 +306,6 @@ ul {
 @media(max-width:600px){
     .section{
         margin-top: 130px;
-    }
-
-    .slide-list{
-        width: 35%;
-    }
-    .slide-list img{
-        width: 50px;
-        height: 50px;
     }
 
     .top-sec-img img{
